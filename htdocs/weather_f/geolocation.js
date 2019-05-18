a09ef9e6b8f6ba4aadfadee92a0350086eaa1f7b
vburn2018 <44521203+vburn2018@users.noreply.github.com>
2019-05-18 23:57:03 +0300

    Delete geolocation.js

--- htdocs/weather_f/geolocation.js
+++
@@ -1,9 +0,0 @@
-class GeoLocation {
-
-    getGeoLocation() {
-        return new Promise((resolve, reject) => {
-                  navigator.geolocation.getCurrentPosition(resolve, reject);
-        })
-    }
-}
-
