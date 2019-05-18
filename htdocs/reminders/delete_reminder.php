c2160983e1ead8210207ca39cff389baecbd1019
vburn2018 <44521203+vburn2018@users.noreply.github.com>
2019-05-18 23:59:23 +0300

    Delete delete_reminder.php

--- htdocs/reminders/delete_reminder.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __DIR__.'/../conf/conf.php';
-
-$sql = 'DELETE FROM reminder WHERE id_reminder='.$_POST['id_reminder'];
-$result = $conn->query($sql) or die('Error, query delete user failed');
-echo "deleted";
-mysqli_close($conn);
-?>
