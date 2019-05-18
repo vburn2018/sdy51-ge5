20fcdfc9e2ab65f8247ec01a7a50604dd0ba3625
vburn2018 <44521203+vburn2018@users.noreply.github.com>
2019-05-18 23:58:30 +0300

    Delete edit_service.php

--- htdocs/social_services/edit_service.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__.'/../conf/conf.php';
-
-$sql = 'SELECT * FROM service WHERE id_service='.$_POST['id_service'];
-$result = $conn->query($sql) or die('Error, edit service failed');
-$row = mysqli_fetch_assoc($result);
-echo json_encode($row);
-mysqli_close($conn);
-?>
