0271ce8b311100e60fa5223fccb71f63c417be41
vburn2018 <44521203+vburn2018@users.noreply.github.com>
2019-05-18 23:59:15 +0300

    Delete edit_reminder.php

--- htdocs/reminders/edit_reminder.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__.'/../conf/conf.php';
-
-$sql = 'SELECT * FROM reminder WHERE id_reminder='.$_POST['id_reminder'];
-$result = $conn->query($sql) or die('Error, edit reminder failed');
-$row = mysqli_fetch_assoc($result);
-echo json_encode($row);
-mysqli_close($conn);
-?>
